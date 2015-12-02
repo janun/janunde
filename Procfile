2f83940af428876e4714776b312369c6a5a67d53
Henrik Kröger <hedwig@riseup.net>
2015-12-02 16:14:51 +0100

    vanilla wagtail configured for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn janunde.wsgi --log-file -
---
+++ janunde/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+for template_engine in TEMPLATES:
+    template_engine['OPTIONS']['debug'] = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '#jzmpp6wgxxl1#v$#zhvd=50ga=zskelu-synaw+qa1#1zk=#r'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
