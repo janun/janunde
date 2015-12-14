091ac05c404327ca40ecaf9816a0f82cea6aa357
Henrik Kröger <hedwig@riseup.net>
2015-12-14 18:20:09 +0100

    integrate navbar into backend
    menuitems will be rendered from existing pages

---
+++ janunde_styleguide/templatetags/janun_tags.py
@@ -0,0 +1,37 @@
+from django import template
+register = template.Library()
+
+
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    return context['request'].site.root_page
+
+def has_menu_children(page):
+    """does the current page have children in the menu?"""
+    return page.get_children().live().in_menu().exists()
+
+@register.inclusion_tag('janunde_styleguide/components/navbar/navbar.html', takes_context=True)
+def navbar(context, parent, calling_page=None):
+    """renders the navbar"""
+    menuitems = parent.get_children().live().in_menu()
+    print(parent)
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
+        'request': context['request'], # needed by pageurl
+    }
+
+@register.inclusion_tag('janunde_styleguide/components/navbar/navbar_children.html', takes_context=True)
+def navbar_children(context, parent):
+    """used by navbar to render the menu children"""
+    menuitems_children = parent.get_children()
+    menuitems_children = menuitems_children.live().in_menu()
+    return {
+        'parent': parent,
+        'menuitems_children': menuitems_children,
+        'request': context['request'], # needed by pageurl
+    }
