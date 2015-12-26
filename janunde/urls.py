0cefef173f42d40e414d9155526c1eece94b8e78
Henrik Kröger <hedwig@riseup.net>
2015-12-26 23:16:13 +0100

    Update to Wagtail 1.3 and Django 1.9

--- janunde/urls.py
+++ janunde/urls.py
@@ -6,6 +6,7 @@ from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 
+from search.views import search
 
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
@@ -13,7 +14,7 @@ urlpatterns = [
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
-    url(r'^search/$', 'search.views.search', name='search'),
+    url(r'^search/$', search, name='search'),
 
     url(r'', include(wagtail_urls)),
 ]
