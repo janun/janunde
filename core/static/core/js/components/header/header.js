a4d1556943af101d639649b7820a3bbe840a91d2
Henrik Kröger <hedwig@riseup.net>
2016-01-24 20:14:24 +0100

    removed jquery dep from video

--- core/static/core/js/components/header/header.js
+++
@@ -1,6 +0,0 @@
-if (window.matchMedia("(min-width: 40em)").matches) {
-  $('video source').each(function () {
-    $(this).attr('src', $(this).attr('data-src'));
-  });
-  $('video').load();
-}
---
+++ core/static/core/js/video.js
@@ -0,0 +1,17 @@
+/**
+ * enable disabled html5 videos on devices greater 64em
+ *
+ * on devices with a screen width greater than 64em
+ * affects all source elements inside video elements
+ * that have a data-src attribute.
+ * sets src to the value of data-src and uses 'load()' on the video element.
+ */
+//document.addEventListener('DOMContentLoaded', function () {
+  if (window.matchMedia("(min-width: 64em)").matches) {
+    var sources = document.querySelectorAll('video source[data-src]');
+    for (i = 0; i < sources.length; ++i) {
+      sources[i].setAttribute('src', sources[i].getAttribute('data-src'));
+      sources[i].parentNode.load();
+    }
+  }
+//});
