4e74c6f89e84e655209c5ef07b604d6c25d97fd3
Henrik Kröger <hedwig@riseup.net>
2016-02-17 16:44:27 +0100

    remove images and videos from richtext
    since this functionality is already provided by the streamfield interface

--- core/wagtail_hooks.py
+++ core/wagtail_hooks.py
@@ -1,17 +1,45 @@
 from wagtail.wagtailcore import hooks
 from wagtail.wagtailimages import image_operations
+from django.utils.html import format_html
 
 
+
+# add the operation to convert an image to jpg to the image tag
 class ConvertToJPGOperation(image_operations.Operation):
     def construct(self):
         pass
-
     def run(self, willow, image):
         willow.original_format = 'jpeg'
-
 
 @hooks.register('register_image_operations')
 def register_image_operations():
     return [
         ('jpg', ConvertToJPGOperation),
     ]
+
+
+# limit the richttext editor
+# no headings
+# no horizontal line
+@hooks.register('insert_editor_js')
+def editor_js():
+    return """
+        <script>
+          halloPlugins = {
+            'halloformat': {},
+            //'halloheadings': {formatBlocks: []},
+            'hallolists': {},
+            //'hallohr': {},
+            'halloreundo': {},
+            'hallowagtaillink': {},
+            'hallorequireparagraphs': {},
+            'hallowagtaildoclink': {},
+          };
+        </script>
+    """
+
+# hide images and embeds
+# they should be done using streamfield
+@hooks.register('insert_editor_css')
+def editor_css():
+    return '<style>.hallowagtailimage, .hallowagtailembeds { display: none; }</style>'
