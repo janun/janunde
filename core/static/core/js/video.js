bea2aa3668b19c16863bd1a7ee30de99432a762c
Henrik Kröger <hedwig@riseup.net>
2016-03-04 16:47:40 +0100

    header assets in better quality
    The header video and background image are now in much better quality.
    There is responsive image loading and responsive video loading.
    
    TODO: implement autamtic resizing using grunt/gulp or whoknows

--- core/static/core/js/video.js
+++ core/static/core/js/video.js
@@ -1,17 +1,15 @@
-/**
- * enable disabled html5 videos on devices greater 64em
- *
- * on devices with a screen width greater than 64em
- * affects all source elements inside video elements
- * that have a data-src attribute.
- * sets src to the value of data-src and uses 'load()' on the video element.
- */
+// video source media query "polyfill"
+// copies data-src to src if no media attribute or media matches
 document.addEventListener('DOMContentLoaded', function () {
-  if (window.matchMedia("(min-width: 64em)").matches) {
-    var sources = document.querySelectorAll('video source[data-src]');
-    for (var i = 0, length=sources.length; i < length; ++i) {
-      sources[i].setAttribute('src', sources[i].getAttribute('data-src'));
-      sources[i].parentNode.load();
+  var sources = document.querySelectorAll('video source[data-src]');
+
+  for (var i = 0, length=sources.length; i < length; ++i) {
+    var source = sources[i];
+    var media = source.getAttribute('media');
+
+    if (!media || window.matchMedia(media).matches) {
+      source.setAttribute('src', source.getAttribute('data-src'));
+      source.parentNode.load();
     }
   }
 });
