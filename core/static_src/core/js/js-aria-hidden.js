15e0642ba16df35171c2e766aa7d7c2459621c45
Henrik Kröger <hedwig@riseup.net>
2016-04-06 17:46:27 +0200

    documentation for js-aria-hidden

--- core/static_src/core/js/js-aria-hidden.js
+++ core/static_src/core/js/js-aria-hidden.js
@@ -1,4 +1,11 @@
-// This sets aria-hidden=true
+// js-aria-hidden
+// This sets aria-hidden depending on mediaquery
+// if the mediaquery matches aria-hidden is set to true
+// if not its set to false
+//
+// @example markup
+//   <div class="js-aria-hidden"
+//        data-js-aria-hidden-media="(max-width: 1023px)"></div>
 function jsAriaHidden(){
   var target = document.querySelector('.js-aria-hidden');
   var media = target.getAttribute('data-js-aria-hidden-media');
@@ -10,6 +17,6 @@ function jsAriaHidden(){
   }
 };
 
-// register event handlers
+// register event handler
 window.addEventListener('resize', jsAriaHidden);
 jsAriaHidden();
