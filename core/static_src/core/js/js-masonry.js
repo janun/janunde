4d582f4b282ba02e249105d00491ccf49d0d61a1
Henrik Kröger <hedwig@riseup.net>
2016-04-08 22:01:57 +0200

    add a media check to js-masonry

--- core/static_src/core/js/js-masonry.js
+++ core/static_src/core/js/js-masonry.js
@@ -1,25 +1,28 @@
 // activate isotope
 // on .js-masonry containers
 (function () {
-  // TODO: use browserify or sth. to keep track of dependencies
+  // TODO: use browserify or sth. to keep track of dependencies?
   //var Isotope = require('isotope-layout');
   //var imagesLoaded = require('imagesLoaded');
 
-  // get all relevant elements
-  var elements = document.querySelectorAll('.js-masonry');
+  // get all relevant container elements
+  var elements = document.querySelectorAll('.js-masonry-dis');
   for (i = 0; i < elements.length; ++i) {
     var element = elements[i];
+    var media = element.getAttribute('data-js-masonry-media');
 
-    // activate isotope
-    var iso = new Isotope(element, {
-      itemSelector: '.js-masonry__element',
-      percentPosition: true,
-      transitionDuration: 0,
-    });
+    if (!media || window.matchMedia(media).matches) {
+      // activate isotope
+      var iso = new Isotope(element, {
+        itemSelector: '.js-masonry__element',
+        percentPosition: true,
+        transitionDuration: 0,
+      });
 
-    // relayout after image loading is complete
-    imagesLoaded(element, function() {
-      iso.layout();
-    });
+      // relayout after image loading is complete
+      imagesLoaded(element, function() {
+        iso.layout();
+      });
+    }
   }
 }());
