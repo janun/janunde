7fd63ca01eb5a92cb81b245eb599e0a2e79982f9
Henrik Kröger <hedwig@riseup.net>
2016-04-15 22:44:40 +0200

    fix bug with js-masonry and more doc

--- core/static_src/core/js/js-masonry--janun.js
+++ core/static_src/core/js/js-masonry--janun.js
@@ -1,12 +1,23 @@
 // activate isotope
-// on .js-masonry containers
+// on .js-masonry--janun containers on .js-masonry__element elements
+//
+// .js-masonry seems already be used by masonry itself thats why the --janun
+//
+// if an attribute of data-js-masonry-media is specified,
+// masonry is only applied if that media query matches
+//
+// @example markup:
+//   <div class="js-masonry--janun" data-js-masonry-media="(min-width: 40rem)">
+//     <div class="js-masonry__element"></div>
+//     <div class="js-masonry__element"></div>
+//   </div>
 (function () {
   // TODO: use browserify or sth. to keep track of dependencies?
   //var Isotope = require('isotope-layout');
   //var imagesLoaded = require('imagesLoaded');
 
   // get all relevant container elements
-  var elements = document.querySelectorAll('.js-masonry-dis');
+  var elements = document.querySelectorAll('.js-masonry--janun');
   for (i = 0; i < elements.length; ++i) {
     var element = elements[i];
     var media = element.getAttribute('data-js-masonry-media');
@@ -15,7 +26,7 @@
       // activate isotope
       var iso = new Isotope(element, {
         itemSelector: '.js-masonry__element',
-        percentPosition: true,
+        //percentPosition: true,
         transitionDuration: 0,
       });
 
