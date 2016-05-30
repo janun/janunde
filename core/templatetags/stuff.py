927ce2faf1e27882e561879b2a51ee3e6f061494
Henrik Kröger <hedwig@riseup.net>
2016-05-31 00:02:39 +0200

    small enhancements

--- core/templatetags/stuff.py
+++ core/templatetags/stuff.py
@@ -2,11 +2,6 @@ from django import template
 register = template.Library()
 from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
 
-
-@register.filter()
-def cut(value, arg):
-    return value.replace(arg, '')
-
 @register.filter()
 def prettyurl(value):
     return value.replace("http://", "").replace("https://", "").replace("www.", "").strip("/")
