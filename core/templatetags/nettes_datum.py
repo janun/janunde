fb293f55f955b3f8293d8e7b721b36daa70553b5
Henrik Kröger <hedwig@riseup.net>
2016-06-21 10:45:27 +0200

    Datumsformate

--- core/templatetags/nettes_datum.py
+++ core/templatetags/nettes_datum.py
@@ -17,38 +17,6 @@ def format_month(date):
         return date_filter(date, "F Y")
 
 
-def is_in_this_week(date):
-    """returns the last day of this week"""
-    today = datetime.date.today()
-    return today <= date <= today + datetime.timedelta(days=6)
-
-
-# def is_in_next_week(date):
-#     """returns the last day of this week"""
-#     end_of_next_week = today + datetime.timedelta(days=6 - today.weekday() + 7)
-#     begin_of_next_week = today + datetime.timedelta(days=7-today.weekday())
-#     return begin_of_next_week <= date <= end_of_next_week
-
-
-def is_in_last_week(date):
-    """returns the last day of this week"""
-    today = datetime.date.today()
-    end_of_last_week = today + datetime.timedelta(days=6 - today.weekday() - 7)
-    begin_of_last_week = today + datetime.timedelta(days=0-today.weekday() - 7)
-    return begin_of_last_week <= date <= end_of_last_week
-
-
-@register.filter(needs_autoescape=True)
-def add_tooltip(date, text, autoescape=True):
-    if autoescape:
-        esc = conditional_escape
-    else:
-        esc = lambda x: x
-    return mark_safe('<span title="{}">{}</span>'.format(
-        date_filter(date, "SHORT_DATE_FORMAT"),
-        esc(text)
-    ))
-
 
 @register.filter(needs_autoescape=True)
 def grey_span(text, autoescape=True):
@@ -59,52 +27,40 @@ def grey_span(text, autoescape=True):
     return mark_safe('<span class="color-grey">{}</span>'.format(esc(text)))
 
 
-@register.filter(expects_localtime=True)
-def nettes_datum_grey_date(our_date):
-    return nettes_datum(our_date, grey_date=True)
-
-
-@register.filter(expects_localtime=True)
-def nettes_datum(our_date, show_date=False, tooltip=True, grey_date=False):
-    """
-    generate a nice looking/readable date in German
-    """
-    today = datetime.date.today()
-    tomorrow = today + datetime.timedelta(days=1)
-    if type(our_date) is datetime.datetime:
-        our_date = our_date.date()
-    wochentag = date_filter(our_date, "l")
-
-    text = ""
-    if is_in_last_week(our_date):
-        text = "letzter {}".format( wochentag )
-    elif our_date == today - datetime.timedelta(days=2):
-        text = "vorgestern"
-    elif our_date == today - datetime.timedelta(days=1):
-        text = "gestern"
-    elif our_date == today:
-        text = "heute"
-    elif our_date == tomorrow:
-        text = "morgen"
-    # elif our_date == tomorrow + datetime.timedelta(days=1):
-    #     text = "übermorgen"
-    elif is_in_this_week(our_date):
-        text = wochentag
-    # elif is_in_next_week(our_date):
-    #     text = "nächsten {}".format( wochentag )
+def datum(value, today):
+    if value.year != today.year:
+        return date_filter(value, "d.m.Y")
     else:
-        tooltip = False # already has date
-        if grey_date:
-            text = mark_safe("{}., {}".format(
-                date_filter(our_date, "D"),
-                grey_span(date_filter(our_date, "d.m."))
-            ))
-        else:
-            text = "{}., {}".format(
-                date_filter(our_date, "D"),
-                date_filter(our_date, "d.m.")
-            )
+        return date_filter(value, "d.m.")
 
-    if tooltip:
-        text = add_tooltip(our_date, text)
-    return text
+
+@register.filter(expects_localtime=True)
+def nettes_datum2(value, short=False):
+    try:
+        tzinfo = getattr(value, 'tzinfo', None)
+        value = datetime.date(value.year, value.month, value.day)
+    except AttributeError:
+        # Passed value wasn't a date object
+        return value
+    except ValueError:
+        # Date arguments out of range
+        return value
+    today = datetime.datetime.now(tzinfo).date()
+    delta = value - today
+    if short:
+        grey_span = lambda x: x
+
+    if delta.days == 0:
+        return mark_safe( "heute" + grey_span( ", " + datum(value, today) ) )
+    elif delta.days == 1:
+        return mark_safe( "morgen" + grey_span( ", " + datum(value, today) ) )
+    elif delta.days == -1:
+        return mark_safe( "gestern" + grey_span( ", " + datum(value, today) ) )
+    else:
+        if delta.days > 7:
+            return date_filter(value, "D") + " " + datum(value, today)
+        else:
+            if short:
+                return date_filter(value, "D") + " " + grey_span( ", " + datum(value, today) )
+            else:
+                return date_filter(value, "l") + ", " + grey_span( ", " + datum(value, today) )
