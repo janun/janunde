1c1c7d33ef0cd142439990024d83728281f8e9c4
Henrik Kröger <hedwig@riseup.net>
2016-06-22 15:37:01 +0200

    fixes resulted from html validation

--- core/templatetags/srcset.py
+++ core/templatetags/srcset.py
@@ -9,9 +9,14 @@ def srcset(image, filter_specs):
     generate a list of image renditions suitable for html5 srcset
     usage:
         srcset="{{ image|srcset:'width-320|jpg width-640|jpg' }}"
+
+    TODO: Don't ouput image renditions that arent any bigger than previous ones,
+          i.e. when the source picture isnt big enough
     """
-    sources = []
+    sources = {}
     for filter_spec in filter_specs.split(' '):
         rendition = get_rendition_or_not_found(image, filter_spec)
-        sources.append( "%s %sw" % (rendition.url, rendition.width) )
-    return ', '.join(sources)
+        if not rendition.width in sources:
+            sources[rendition.width] = "%s %sw" % (rendition.url, rendition.width)
+
+    return ', '.join(sources.values())
