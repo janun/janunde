fe3cfeedc0a37ed7dcf3f25b7f1329177cc6a55b
Henrik Kröger <hedwig@riseup.net>
2016-12-22 17:00:53 +0100

    several-images Bilderstreifen block

--- core/blocks.py
+++ core/blocks.py
@@ -45,6 +45,52 @@ class ImageBlock(blocks.StructBlock):
         icon = "image"
 
 
+class ImageCarouselBlock(blocks.StructBlock):
+    image = ImageChooserBlock(
+        label="Bild"
+    )
+    caption = blocks.CharBlock(
+        label="opt. Beschrift.",
+        required=False
+    )
+    class Meta:
+        icon = 'image'
+        label = 'Bild'
+
+
+class ImagesBlock(blocks.ListBlock):
+
+    def get_context(self, value):
+       context = super(ImagesBlock, self).get_context(value)
+       count = len(context['self'])
+       context['aspect'] = 66.66
+       context['srcset'] = 'fill-300x200 fill-600x400 fill-900x600 fill-1200x800'
+       if count == 1:
+           context['srcset'] = 'width-320 width-640 width-960 width-1280'
+           context['sizes'] = '(min-width: 830px) 830px, 100vw'
+           context['multiple'] = 1
+           context['aspect'] = context['self'][0].get('image').aspect
+       elif count % 4 == 0:
+            context['sizes'] = '(min-width: 830px) 208px, 100vw'
+            context['multiple'] = 4
+       elif count % 3 == 0 or count in (5,7,10):
+           context['sizes'] = '(min-width: 830px) 267px, 100vw'
+           context['multiple'] = 3
+       elif count % 2 == 0:
+           context['sizes'] = '(min-width: 830px) 405px, 100vw'
+           context['multiple'] = 2
+       else:
+           context['sizes'] = '(min-width: 830px) 405px, 50vw'
+           context['multiple'] = 2
+       return context
+
+    class Meta:
+        template = "blocks/several_images.html",
+        label = "Bilder"
+        icon = "image"
+
+
+
 class Button(blocks.StructBlock):
     text = blocks.CharBlock(label="Text")
     link = blocks.URLBlock(label="Link")
@@ -87,10 +133,10 @@ class ParagraphBlock(blocks.RichTextBlock):
         template = 'blocks/paragraph.html'
 
 
-
 class StandardStreamBlock(blocks.StreamBlock):
     paragraph = ParagraphBlock()
     image = ImageBlock()
+    several_images = ImagesBlock(ImageCarouselBlock)
     embed = OurEmbedBlock()
     button = Button()
     attachment = Attachment()
