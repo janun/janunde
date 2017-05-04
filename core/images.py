d4c36b083ef7e621f67f81397985ccb9fc1ea5df
Henrik Kröger <hedwig@riseup.net>
2017-05-04 19:11:49 +0200

    bump django and wagtail

--- core/images.py
+++ core/images.py
@@ -36,24 +36,3 @@ class AttributedRendition(AbstractRendition):
         unique_together = (
             ('image', 'filter_spec', 'focal_point_key'),
         )
-
-
-# Delete the source image file when an image is deleted
-@receiver(pre_delete, sender=AttributedImage)
-def image_delete(sender, instance, **kwargs):
-    instance.file.delete(False)
-
-
-# Delete the rendition image file when a rendition is deleted
-@receiver(pre_delete, sender=AttributedRendition)
-def rendition_delete(sender, instance, **kwargs):
-    instance.file.delete(False)
-
-
-# Do feature detection when a user saves an image without a focal point
-# @receiver(pre_save, sender=AttributedImage)
-# def image_feature_detection(sender, instance, **kwargs):
-#    # Make sure the image doesn't already have a focal point
-#    if not instance.has_focal_point():
-#        # Set the focal point
-#        instance.set_focal_point(instance.get_suggested_focal_point())
