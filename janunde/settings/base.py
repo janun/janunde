7ba54537ec973c4408383efcc32170c189d6f7cb
Henrik Kröger <hedwig@janun.de>
2019-12-14 17:11:08 +0100

    janunde/static not used without bower

--- janunde/settings/base.py
+++ janunde/settings/base.py
@@ -141,7 +141,7 @@ STATICFILES_FINDERS = (
 )
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, "static"),
+    # os.path.join(PROJECT_DIR, "static"),
     os.path.join(BASE_DIR, "node_modules"),
 )
 
