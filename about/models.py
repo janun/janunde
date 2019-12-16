f32c6dd86ea37bab517fcd6c43a9d3cd83c91068
Henrik Kröger <hedwig@janun.de>
2019-12-16 17:44:00 +0100

    code style

--- about/models.py
+++ about/models.py
@@ -1,52 +1,37 @@
-from django.db import models
-from django.utils.text import slugify
+from django.utils.text import Truncator
 
 from wagtail.core.fields import StreamField
-from wagtail.admin.edit_handlers import (FieldPanel, InlinePanel,
-                                                ObjectList, PageChooserPanel,
-                                                StreamFieldPanel, FieldRowPanel,
-                                                TabbedInterface, MultiFieldPanel)
-from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.search import index
-from wagtail.core.fields import RichTextField
-from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel
 
+import html2text
 
 from core.blocks import StandardStreamBlock
 from core.models import HeaderMixin, BasePage
-from core.images import AttributedImage as Image
-
 
 
 class AboutPage(BasePage, HeaderMixin):
 
-    content = StreamField(
-        StandardStreamBlock(),
-        blank=True,
-        verbose_name="Inhalt",
-    )
+    content = StreamField(StandardStreamBlock(), blank=True, verbose_name="Inhalt",)
 
     content_panels = [
-        FieldPanel('title'),
+        FieldPanel("title"),
         MultiFieldPanel(HeaderMixin.panels, "Header"),
-        StreamFieldPanel('content'),
+        StreamFieldPanel("content"),
     ]
 
     def get_image(self):
         if super().get_image():
             return super().get_image()
         for block in self.content:
-            if block.block_type == 'image':
-                return block.value['image']
+            if block.block_type == "image":
+                return block.value["image"]
 
     def get_description(self):
         if self.search_description:
             return self.search_description
-        from django.utils.text import Truncator
-        import html2text
         h = html2text.HTML2Text()
         h.ignore_links = True
         for block in self.content:
-            if block.block_type == 'paragraph':
+            if block.block_type == "paragraph":
                 text = h.handle(block.value.source)
                 return Truncator(text).words(25)
--- contact/models.py
+++ contact/models.py
@@ -1,83 +1,53 @@
 from django.db import models
-from django.utils.text import slugify
 
 from wagtail.core.fields import StreamField
-from wagtail.admin.edit_handlers import (FieldPanel, InlinePanel,
-                                                ObjectList, PageChooserPanel,
-                                                StreamFieldPanel, FieldRowPanel,
-                                                TabbedInterface, MultiFieldPanel)
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
-
-from modelcluster.fields import ParentalKey
-from wagtail.core.models import Orderable, PageManager
-
-from core.blocks import StandardStreamBlock
-from core.models import BasePage, HeaderMixin
-from core.images import AttributedImage as Image
+from wagtail.core.models import PageManager
 
 from phonenumber_field.modelfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
 
-
-
+from core.forms import ShortTitleForm
+from core.blocks import StandardStreamBlock
+from core.models import BasePage, HeaderMixin
+from core.images import AttributedImage as Image
 
 
 class ContactIndex(BasePage, HeaderMixin):
-    subpage_types = ['PersonPage', 'OfficePage']
-    parent_page_types = ['core.HomePage']
+    subpage_types = ["PersonPage", "OfficePage"]
+    parent_page_types = ["core.HomePage"]
 
     class Meta:
         verbose_name = "Auflistung von Kontakten"
 
     def get_context(self, request):
         context = super().get_context(request)
-        context['people'] = PersonPage.objects.descendant_of(self).live()
-        context['offices'] = OfficePage.objects.descendant_of(self).live()
+        context["people"] = PersonPage.objects.descendant_of(self).live()
+        context["offices"] = OfficePage.objects.descendant_of(self).live()
         return context
 
     content_panels = [
-        FieldPanel('title'),
+        FieldPanel("title"),
         MultiFieldPanel(HeaderMixin.panels, "Header"),
     ]
 
 
-
-
-
-
-# class PersonPageContactPossibility(Orderable):
-#     page = ParentalKey(PersonPage, related_name='contact_possibilities')
-#     contact = models.CharField(max_length=255)
-#
-#     panels = [
-#         FieldPanel('contact'),
-#     ]
-
-
 class OfficePageManager(PageManager):
     def get_queryset(self):
-         return super().get_queryset().order_by('title')
+        return super().get_queryset().order_by("title")
 
-from core.forms import ShortTitleForm
 
 class OfficePage(BasePage):
     partial_template_name = "contact/_office.html"
     base_form_class = ShortTitleForm
 
     photo = models.ForeignKey(
-        Image,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
+        Image, null=True, blank=True, on_delete=models.SET_NULL, related_name="+"
     )
 
-    text = StreamField(
-        StandardStreamBlock(),
-        blank=True,
-        verbose_name="Text",
-    )
+    text = StreamField(StandardStreamBlock(), blank=True, verbose_name="Text",)
 
     mail = models.EmailField("E-Mail", blank=True)
     phone = PhoneNumberField("Telefonnummer", blank=True)
@@ -86,19 +56,18 @@ class OfficePage(BasePage):
     objects = OfficePageManager()
 
     content_panels = [
-        FieldPanel('title'),
-        ImageChooserPanel('photo'),
-        FieldPanel('address'),
-        FieldPanel('mail'),
-        FieldPanel('phone', widget=PhoneNumberInternationalFallbackWidget),
-        StreamFieldPanel('text'),
+        FieldPanel("title"),
+        ImageChooserPanel("photo"),
+        FieldPanel("address"),
+        FieldPanel("mail"),
+        FieldPanel("phone", widget=PhoneNumberInternationalFallbackWidget),
+        StreamFieldPanel("text"),
     ]
 
     search_fields = BasePage.search_fields + [
-        index.SearchField('text'),
-        index.SearchField('mail'),
-        #index.SearchField('phone'), # currently unable to serialize
-        index.SearchField('address'),
+        index.SearchField("text"),
+        index.SearchField("mail"),
+        index.SearchField("address"),
     ]
 
     class Meta:
@@ -108,16 +77,13 @@ class OfficePage(BasePage):
 
 class PersonPageManager(PageManager):
     def get_queryset(self):
-         return super().get_queryset().order_by('title')
+        return super().get_queryset().order_by("title")
+
 
 class PersonPage(BasePage):
     partial_template_name = "contact/_person.html"
 
-    text = StreamField(
-        StandardStreamBlock(),
-        blank=True,
-        verbose_name="Text",
-    )
+    text = StreamField(StandardStreamBlock(), blank=True, verbose_name="Text",)
     role = models.CharField("Rolle", max_length=255, blank=True)
     mail = models.EmailField("E-Mail", blank=True)
     phone = PhoneNumberField("Telefonnummer", blank=True)
@@ -126,36 +92,31 @@ class PersonPage(BasePage):
         OfficePage,
         null=True,
         blank=True,
-        related_name='people',
+        related_name="people",
         on_delete=models.SET_NULL,
         verbose_name="Zugehöriges Büro",
     )
 
     photo = models.ForeignKey(
-        Image,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
+        Image, null=True, blank=True, on_delete=models.SET_NULL, related_name="+"
     )
 
     objects = PersonPageManager()
 
     search_fields = BasePage.search_fields + [
-        index.SearchField('text'),
-        index.SearchField('mail'),
-        #index.SearchField('phone'), # currently unable to serialize
-        index.SearchField('role'),
+        index.SearchField("text"),
+        index.SearchField("mail"),
+        index.SearchField("role"),
     ]
 
     content_panels = [
-        FieldPanel('title'),
-        ImageChooserPanel('photo'),
-        FieldPanel('role'),
-        FieldPanel('mail'),
-        FieldPanel('phone', widget=PhoneNumberInternationalFallbackWidget),
-        StreamFieldPanel('text'),
-        FieldPanel('office'),
+        FieldPanel("title"),
+        ImageChooserPanel("photo"),
+        FieldPanel("role"),
+        FieldPanel("mail"),
+        FieldPanel("phone", widget=PhoneNumberInternationalFallbackWidget),
+        StreamFieldPanel("text"),
+        FieldPanel("office"),
     ]
 
     class Meta:
