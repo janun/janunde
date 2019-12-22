988eba2061c13d73bff658db9f7633e76d7d2b21
Henrik Kröger <hedwig@janun.de>
2019-12-22 16:45:58 +0100

    Remove unneeded jpg convert
    format-jpeg was added upstream

--- core/wagtail_hooks.py
+++ core/wagtail_hooks.py
@@ -1,24 +1,6 @@
-from wagtail.core import hooks
-from wagtail.images import image_operations
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
 from .models import Article
-
-
-# add the operation to convert an image to jpg to the image tag
-class ConvertToJPGOperation(image_operations.Operation):
-    def construct(self):
-        pass
-
-    def run(self, willow, image):
-        willow.original_format = "jpeg"
-
-
-@hooks.register("register_image_operations")
-def register_image_operations():
-    return [
-        ("jpg", ConvertToJPGOperation),
-    ]
 
 
 class ArticleModelAdmin(ModelAdmin):
