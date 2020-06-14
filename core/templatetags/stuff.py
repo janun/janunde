b61f8b688a7d3122fc3a2d1bf9cd0253c7fb529f
Henrik Kröger <hedwig@janun.de>
2020-06-14 12:54:14 +0200

    add template tag to render dict to json-ld

--- core/templatetags/stuff.py
+++ core/templatetags/stuff.py
@@ -1,9 +1,11 @@
 import urllib
+import json
 
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 from django.http import HttpRequest
+from django.core.serializers.json import DjangoJSONEncoder
 
 from wagtail.embeds import embeds
 from wagtail.embeds.exceptions import EmbedException
@@ -15,6 +17,7 @@ register = template.Library()
 
 @register.filter()
 def prettyurl(value):
+    """return only the domain part of an url"""
     return urllib.parse.urlparse(value).netloc
 
 
@@ -31,9 +34,9 @@ def get_embed(url, max_width=None):
         return ""
 
 
-# Banner snippets
 @register.inclusion_tag("core/banners.html", takes_context=True)
 def banners(context):
+    """Render banners"""
     return {
         "banners": Banner.objects.all(),
         "request": context["request"],
@@ -90,3 +93,14 @@ def highlight(value, search, klass="font-bold"):
 def ancestors(obj):
     """Get the ancestors of obj excluding Root and Homepage"""
     return obj.get_ancestors()[2:]
+
+
+@register.simple_tag(takes_context=True)
+def render_json_ld(context, structured_data: dict) -> str:
+    """Render JSON-LD from dict"""
+    structured_data["@context"] = "https://schema.org"
+    dumped = json.dumps(
+        structured_data, cls=DjangoJSONEncoder, ensure_ascii=False, sort_keys=True
+    )
+    text = f"""<script type="application/ld+json">{dumped}</script>"""
+    return mark_safe(text)
