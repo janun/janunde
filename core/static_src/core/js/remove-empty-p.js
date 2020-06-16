508fac5d24fcdea277cfafc9c2b74d206e3cf230
Henrik Kröger <hedwig@janun.de>
2020-06-16 15:08:28 +0200

    remove unused custom js

--- core/static_src/core/js/remove-empty-p.js
+++
@@ -1,9 +0,0 @@
-// the halo.js editor likes to create blank p tags...
-// but margin-bottom of p tags breaks layout.
-$(document).ready(function () {
-  $('p').each(function() {
-      var $this = $(this);
-      if($this.html().replace(/\s|&nbsp;/g, '').length == 0)
-          $this.remove();
-  });
-});
