3278032948364812558539bdf45144b71e3aa9d8
Henrik Kröger <hedwig@janun.de>
2020-07-25 17:51:37 +0200

    add check if banners
    fighting a strange bug

--- banners/templatetags/banner_tags.py
+++ banners/templatetags/banner_tags.py
@@ -8,7 +8,10 @@ register = template.Library()
 @register.inclusion_tag("banners/banners.html", takes_context=True)
 def banners(context):
     """Render banners"""
-    return {
-        "banners": Banner.get_by_request(context["request"]),
-        "request": context["request"],
-    }
+    request = context.get("request", None)
+    if request:
+        return {
+            "banners": Banner.get_by_request(request),
+            "request": request,
+        }
+    return {}
